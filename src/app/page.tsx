33a90290d31c5489d78ebd56146190934a1d17b0
blakenp <blake.ptrsn18@gmail.com>
2024-02-06 02:15:37 -0700

    add finishing touching to styling and add simple navbar and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -124,6 +124,141 @@ export default function Home() {
           quality={100}
           className="Image"
         />
+        <p className="note">
+          Note: You might be confused as to why we used () in our code. 
+          It may seem unnecessary, but whenever we enter a function, 
+          Python needs parentheses to recognize it. 
+        </p>
+        <h2>Step 5: Calculate The Percentages</h2>
+        <p>
+          In a new code chunk, use these commands to <b>calculate the percentages</b> of
+          how many people listed each character as their favorite. Click play.
+        </p>
+        <Image
+          src={"/percentages_calc.png"}
+          width={700}
+          height={150}
+          alt="Percentages Calc"
+          priority={true}
+          quality={100}
+          className="Image"
+        />
+        <p className="note">
+          Note: These lines of code first count how many people listed each 
+          character as their favorite, and then convert these counts into percentages. 
+          You may notice that the British spelling of the word favorite <b>("favourite")</b> is used. 
+          This is because the dataset we imported is British, 
+          and this code specifically refers Python to a part of that dataset.
+        </p>
+        <p>
+          Python should return these data entries: 
+        </p>
+        <Image
+          src={"/percentages_display.png"}
+          width={700}
+          height={170}
+          alt="Percentages Display"
+          priority={true}
+          quality={100}
+          className="Image"
+        />
+        <h2>Step 6: Create the Bar Chart</h2>
+        <p>
+          In a new code chunk, type these lines of code to <b>create the desired bar chart</b>. Click play.
+        </p>
+        <Image
+          src={"/plotting_logic.png"}
+          width={700}
+          height={100}
+          alt="Plotting Logic"
+          quality={100}
+          className="Image"
+        />
+        <p>
+          If you do this step correctly, you should get an image like this:
+        </p>
+        <Image
+          src={"/plot_display1.png"}
+          width={700}
+          height={400}
+          alt="Plot 1"
+          quality={100}
+          className="Image"
+        />
+        <p className="note">
+          Note: This code tells Python what kind of graph to make, and how big to make it. 
+        </p>
+        <h2>Step 7: Label Your Graph</h2>
+        <p>
+          In the <b>SAME</b> code chunk, add the following commands (highlighted in yellow)
+          to <b>create labels on your graph</b>. Click play.
+        </p>
+        <Image
+          src={"/plt_labeling.png"}
+          width={700}
+          height={200}
+          alt="Plot 1"
+          priority={true}
+          quality={100}
+          className="Image"
+        />
+        <p>
+          You are all done! The final product should look something like this:
+        </p>
+        <Image
+          src={"/plot_display2.png"}
+          width={700}
+          height={400}
+          alt="Plot 2"
+          quality={100}
+          className="Image"
+        />
+        <p className="note">
+          Note: You can choose whatever color you like for your graph! 
+          Try typing the name of a different color in the color='' space (line 2 of the code).
+          If you choose a color that Python doesn't recognize, 
+          it will give you an error message when you click play. 
+          You also may notice that we used the American spelling of favorite here. 
+          We can make the titles whatever we want, and we're assuming we have an American audience.
+        </p>
+        <h1>Troubleshooting</h1>
+        <h3>
+          If you get an error message, don't panic. 
+          Check to make sure you typed every line of code exactly as instructed. Additionally:
+        </h3>
+        <ul>
+          <li>
+            One common error that causes Python code not to run is incorrect indentation. 
+            Make sure if you have a for loop 
+            (a function that uses the word for to tell Python to execute a command repeatedly) 
+            that the following line is indented with tab once. An example is below:
+          </li>
+          <Image
+            src={"/for_loop_example.png"}
+            width={140}
+            height={64}
+            alt="For Loop Example"
+            quality={100}
+            className="Image"
+          />
+          <li>
+            Forgetting to place a colon at the end of a for loop will also cause
+            Python to throw errors. Again, refer to the for loop above.
+          </li>
+          <li>
+            The cells containing code must be run sequentially or certain cells 
+            that depend on the previous cells will not execute the code.
+          </li>
+        </ul>
+        <h1>Conclusion</h1>
+        <p>
+          After you have written and run the last chunk of code, 
+          make sure that there are no errors and that you get the desired results. 
+          You should see a horizontal bar graph comparing favorite Harry Potter characters.
+        </p>
+        <p>
+          Your program is now finished, and you are ready to create bar graphs in Python!
+        </p>
       </div>
     </main>
   );

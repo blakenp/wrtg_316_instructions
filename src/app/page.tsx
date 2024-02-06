0f5327ef776fcf1c1d84741b85132827fce62193
blakenp <blake.ptrsn18@gmail.com>
2024-02-05 22:34:10 -0700

    add global styling for p, header tags, and lists and use global div-container to center text on the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,26 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p>Hello</p>
+    <main>
+      <div className="div-container">
+        <h1>How To Create a Bar Graph in Python</h1>
+        <p>
+          The ability to create graphs that communicate data is one of the most
+          important tools for a data scientist to have.
+          These instructions will walk you through the process, step by step,
+          until you feel confident creating a simple bar graph in Python!
+        </p>
+        <h3>Materials:</h3>
+        <ul>
+          <li>Computer (Provided)</li>
+          <li>Google Colab (Provided)</li>
+          <li>A data set (Provided)</li>
+          <li>Hands to type</li>
+        </ul>
         <Image 
-          src={'/image1.png'}
-          width={500}
-          height={500}
+          src={'/add_code_button.png'}
+          width={700}
+          height={370}
           alt="Step 1 Image"
         />
       </div>
